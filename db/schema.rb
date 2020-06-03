c52eda151c67f64da668e8655ce274a0e9186a08
Chantelle Isaacs <chantelle616@gmail.com>
2020-06-03 16:56:05 -0700

    adding sightings and figuring out how to show sightings between certain times

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_03_174606) do
+ActiveRecord::Schema.define(version: 2020_06_03_225359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 2020_06_03_174606) do
     t.string "kingdom"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "sightings", force: :cascade do |t|
+    t.datetime "date"
+    t.float "latitude"
+    t.float "longitude"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "animal_id"
   end
 
 end
